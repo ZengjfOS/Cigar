2c1ea6dea52bdb75351c8581501c7ca27632293c
ZoroZeng <aplexos.@163.com>
2018-03-19 14:14:44 +0800

    init

---
+++ cigarOut/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace cigarOut
+{
+    partial class cigarOut
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.header = new System.Windows.Forms.Label();
+            this.telegraphNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.telegraphContent = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.telegraphSize = new System.Windows.Forms.Label();
+            this.sendStrings = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.lab = new System.Windows.Forms.Label();
+            this.ip = new System.Windows.Forms.TextBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // header
+            // 
+            this.header.AutoSize = true;
+            this.header.Location = new System.Drawing.Point(12, 61);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(65, 12);
+            this.header.TabIndex = 0;
+            this.header.Text = "电文编号：";
+            // 
+            // telegraphNumber
+            // 
+            this.telegraphNumber.Location = new System.Drawing.Point(71, 58);
+            this.telegraphNumber.Name = "telegraphNumber";
+            this.telegraphNumber.Size = new System.Drawing.Size(117, 21);
+            this.telegraphNumber.TabIndex = 1;
+            this.telegraphNumber.Text = "C1T101";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "数据内容：";
+            // 
+            // telegraphContent
+            // 
+            this.telegraphContent.Location = new System.Drawing.Point(71, 90);
+            this.telegraphContent.Name = "telegraphContent";
+            this.telegraphContent.Size = new System.Drawing.Size(353, 21);
+            this.telegraphContent.TabIndex = 1;
+            this.telegraphContent.Text = "RUN04530820151202121130";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "电文长度：";
+            // 
+            // telegraphSize
+            // 
+            this.telegraphSize.AutoSize = true;
+            this.telegraphSize.Location = new System.Drawing.Point(83, 123);
+            this.telegraphSize.Name = "telegraphSize";
+            this.telegraphSize.Size = new System.Drawing.Size(11, 12);
+            this.telegraphSize.TabIndex = 2;
+            this.telegraphSize.Text = "0";
+            // 
+            // sendStrings
+            // 
+            this.sendStrings.Location = new System.Drawing.Point(14, 153);
+            this.sendStrings.Multiline = true;
+            this.sendStrings.Name = "sendStrings";
+            this.sendStrings.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.sendStrings.Size = new System.Drawing.Size(410, 155);
+            this.sendStrings.TabIndex = 3;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(349, 29);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 43);
+            this.send.TabIndex = 4;
+            this.send.Text = "Send";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Location = new System.Drawing.Point(12, 30);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(29, 12);
+            this.lab.TabIndex = 0;
+            this.lab.Text = "IP：";
+            this.lab.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // ip
+            // 
+            this.ip.Location = new System.Drawing.Point(71, 27);
+            this.ip.Name = "ip";
+            this.ip.Size = new System.Drawing.Size(117, 21);
+            this.ip.TabIndex = 1;
+            this.ip.Text = "10.130.161.59";
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(227, 27);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(75, 46);
+            this.connect.TabIndex = 4;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // cigarOut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 320);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.sendStrings);
+            this.Controls.Add(this.telegraphSize);
+            this.Controls.Add(this.telegraphContent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ip);
+            this.Controls.Add(this.telegraphNumber);
+            this.Controls.Add(this.lab);
+            this.Controls.Add(this.header);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "cigarOut";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CigarOut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label header;
+        private System.Windows.Forms.TextBox telegraphNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox telegraphContent;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label telegraphSize;
+        private System.Windows.Forms.TextBox sendStrings;
+        private System.Windows.Forms.Button send;
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.TextBox ip;
+        private System.Windows.Forms.Button connect;
+    }
+}
+
---
+++ cigarOut/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace cigarOut
+{
+    public partial class cigarOut : Form
+    {
+        Socket clientSocket = null;
+
+        /**
+         * 0 : don't connect
+         * 1 : conect 
+         * 2 : send except
+         * 3 : receive except
+         */
+        int conectStatus = 0;
+
+        public cigarOut()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (connect.Text.Equals("Connect"))
+            {
+
+                IPAddress ipaddr = IPAddress.Parse(ip.Text);
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    clientSocket.Connect(new IPEndPoint(ipaddr, 8088)); //配置服务器IP与端口  
+                    this.conectStatus = 1;
+                    Console.WriteLine("连接服务器成功");
+                }
+                catch
+                {
+                    Console.WriteLine("连接服务器失败，请按回车键退出！");
+                    return;
+                }
+
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start(clientSocket);
+
+                Thread sendThread = new Thread(SendMessage);
+                sendThread.Start(clientSocket);
+
+                connect.Text = "Close";
+            }
+            else
+            {
+                if (clientSocket != null)
+                {
+                    clientSocket.Close();
+                    clientSocket = null;
+                }
+
+                connect.Text = "Connect";
+            }
+        }
+        /// <summary>  
+        /// 接收消息  
+        /// </summary>  
+        /// <param name="clientSocket"></param>  
+        private void ReceiveMessage(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            byte[] result = new byte[1024];
+            // 设置20s未接收到心跳包超时，会引发异常
+            myClientSocket.ReceiveTimeout = 20000;
+
+            while (true)
+            {
+                try
+                {
+                    //通过clientSocket接收数据  
+                    int receiveNumber = myClientSocket.Receive(result);
+                    if (receiveNumber != 0)
+                    {
+                        Action<String> AsyncUIDelegate1 = delegate(string n) { sendStrings.AppendText(n); };
+                        telegraphContent.Invoke(AsyncUIDelegate1, "Receive " + receiveNumber + " bytes: " + Encoding.ASCII.GetString(result, 0, receiveNumber));
+                    }
+                    else
+                    {
+                        this.conectStatus = 3;
+                        connect.Text = "Connect";
+                        clientSocket = null;
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("connect receive data except.");
+
+                    switch (this.conectStatus)
+                    {
+                        case 1:
+                            this.conectStatus = 3;
+                            Action<String> AsyncUIDelegate1 = delegate(string n) { connect.Enabled = false;};
+                            telegraphContent.Invoke(AsyncUIDelegate1, "");
+                            break;
+                        case 2:
+                            Action<String> AsyncUIDelegate2 = delegate(string n) { connect.Text = "Connect"; connect.Refresh(); connect.Enabled = true;};
+                            telegraphContent.Invoke(AsyncUIDelegate2, "");
+                            clientSocket = null;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        /// <summary>  
+        /// 接收消息  
+        /// </summary>  
+        /// <param name="clientSocket"></param>  
+        private void SendMessage(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            byte[] result = new byte[1024];
+            // 设置20s未接收到心跳包超时，会引发异常
+            myClientSocket.ReceiveTimeout = 20000;
+
+            while (true)
+            {
+                try
+                {
+                    string sendString = "C1T1990002\r\n";
+                    myClientSocket.Send(System.Text.Encoding.Default.GetBytes(sendString));
+
+                    Action<String> AsyncUIDelegate1 = delegate(string n) { sendStrings.AppendText(n); };
+                    telegraphContent.Invoke(AsyncUIDelegate1, "Send " + sendString.Length + " bytes: " + sendString);
+
+                    Thread.Sleep(10000);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("connect send heatbeat data except.");
+
+                    switch (this.conectStatus)
+                    {
+                        case 1:
+                            this.conectStatus = 2;
+                            Action<String> AsyncUIDelegate1 = delegate(string n) { connect.Enabled = false;};
+                            telegraphContent.Invoke(AsyncUIDelegate1, "");
+                            break;
+                        case 3:
+                            Action<String> AsyncUIDelegate2 = delegate(string n) { connect.Text = "Connect"; connect.Refresh(); connect.Enabled = true;};
+                            telegraphContent.Invoke(AsyncUIDelegate2, "");
+                            clientSocket = null;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            if (clientSocket == null)
+                return;
+
+            try
+            {
+                StringBuilder sendString = new StringBuilder();
+
+                int count = 0;
+                String content = "";
+                String header = "";
+                Action<String> AsyncUIDelegate = delegate(string n) { 
+                    header = telegraphNumber.Text; 
+                    count = telegraphContent.TextLength + 2;  // 2 just for end of a line "\r\n："
+                    content = telegraphContent.Text;
+                    telegraphSize.Text = count.ToString();
+                };
+                telegraphContent.Invoke(AsyncUIDelegate, "");
+
+                sendString.Append(header);
+                sendString.Append(String.Format("{0:0000}", count));
+                sendString.Append(content);
+                sendString.Append("\r\n");
+
+                // Console.WriteLine(sendString);
+
+                //通过clientSocket接收数据  
+                
+                // int receiveNumber = myClientSocket.Send(result);
+                // Console.WriteLine("接收客户端{0}消息{1}", myClientSocket.RemoteEndPoint.ToString(), Encoding.ASCII.GetString(result, 0, receiveNumber));
+
+                clientSocket.Send(System.Text.Encoding.Default.GetBytes(sendString.ToString()));
+
+                Action<String> AsyncUIDelegate1 = delegate(string n) { sendStrings.AppendText(n); };
+                telegraphContent.Invoke(AsyncUIDelegate1, "Send " + sendString.Length + " bytes: " + sendString);
+
+                Console.WriteLine("zengjf");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
